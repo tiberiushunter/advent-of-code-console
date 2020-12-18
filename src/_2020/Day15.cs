09b145c14e77ed6a1292f6d2cefa44458ef7109e
Sam Welek <s.welek@outlook.com>
2020-12-18 16:12:50 +0000

    Adding Day 15 solution

---
+++ src/_2020/Day15.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Linq;
+
+namespace AdventOfCode._2020
+{
+    class Day15 : DayBase
+    {
+        private readonly string _input;
+        private int[] _startingNumbers;
+
+        /// <summary>
+        /// --- Day 15: Rambunctious Recitation ---
+        /// </summary>
+        public Day15()
+        {
+            _input = Program.GetInput(2020, 15);
+            _startingNumbers = _input.Split(',').Select(n => Convert.ToInt32(n)).ToArray();
+        }
+
+        /// <summary>
+        /// --- Day 15: Rambunctious Recitation; Part A ---
+        /// </summary>
+        private protected override string PartA()
+        {
+            return GetSpokenNumberAtTurn(2020).ToString();
+        }
+
+        /// <summary>
+        /// --- Day 15: Rambunctious Recitation; Part B ---
+        /// </summary>
+        private protected override string PartB()
+        {
+            return GetSpokenNumberAtTurn(30000000).ToString();
+        }
+
+        private int GetSpokenNumberAtTurn(int numOfTurns)
+        {
+            int[] spokenNums = new int[numOfTurns];
+            int current = 0;
+
+            // First deal with initial numbers spoken
+            for (int i = 0; i < _startingNumbers.Length - 1; i++)
+            {
+                spokenNums[_startingNumbers[i]] = i + 1;
+                current = _startingNumbers[i + 1];
+            }
+            
+            for (int i = _startingNumbers.Length - 1; i < numOfTurns - 1; i++)
+            {
+                int spokenNum = spokenNums[current];
+                spokenNums[current] = i + 1;
+                current = spokenNum == 0 ? 0 : i + 1 - spokenNum;
+            }
+
+            return current;
+        }
+    }
+}
